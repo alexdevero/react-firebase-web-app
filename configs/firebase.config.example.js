6314e344531c7114b6e090117b23fcba5eb77da5
Alex Devero <deveroalex@gmail.com>
2019-04-04 06:52:52 +0200

    Update firebase config example

---
+++ configs/firebase.config.example.js
@@ -0,0 +1,10 @@
+// Rename this file to firebase.config.js before use
+
+export const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+}
